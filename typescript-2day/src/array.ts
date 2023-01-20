ee4ae3570e1e3733969d8c6f692b529163da3bb9
ByunJungMin <cvy5375@naver.com>
2023-01-20 11:12:53 +0900

    typescript pokemon part1

--- typescript-2day/src/array.ts
+++ typescript-2day/src/array.ts
@@ -2,7 +2,7 @@ let list: number[] = [1, 2, 3];
 
 let list1: string[] = ["안", "녕", "하", "세", "요"];
 
-// 제네릭 타입으로 지정해준 경우 / number[] 와 Array<number> 는 같은 의미
+// 제네릭 타입으로 지정해준 경우
 let list2: Array<number> = [1, 2, 3];
 
 let list3: Array<string> = ["안", "녕"];
--- typescript-2day/src/string.ts
+++ typescript-2day/src/string.ts
@@ -8,4 +8,4 @@ let age: number = 37;
 let sentence: string = `Hello, my name is ${ fullName }.
 I'll be ${ age + 1 } years old next month.`;
 
-console.log(sentence)
+console.log(color)
